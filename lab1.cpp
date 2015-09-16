6a0705ff8ac946b3851b23e981a26df87e923021
agentzlo <agent_zoom@mail.ru>
2015-09-16 20:40:59 +0400

    Started lab1 implementation

--- lab1.cpp
+++ lab1.cpp
@@ -1,3 +1,34 @@
+#import <string.h>
+
+class Book {
+	string author;
+	string title;
+	int year;
+	
+	public:
+		Book(string myAuthor, string myTitle, int myYear)
+}
+
+Book::Book(string myAuthor, string myTitle, int myYear) {
+	author = myAuthor;
+	title = myTitle;
+	year = myYear;
+}
+
 int main() {
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i > n; i++) {
+		string myAuthor;
+		string myTitle;
+		int myYear;
+		
+		cin >> myAuthor >> myTitle >> myYear;
+		
+		Book book(myAuthor, myTitle, myYear);
+	}
+	
 	return 0;
 }
